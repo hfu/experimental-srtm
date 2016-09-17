ef0f2edb986175379c225a7d3d9f0a9869b80c3a
Hidenori FUJIMURA <hfu@si.local>
2016-09-17 09:32:25 +0900

    update

--- download.rb
+++ download.rb
@@ -2,17 +2,31 @@ require 'open-uri'
 require 'fileutils'
 all = 87381
 count = 0
-0.upto(8) {|z|
+
+def lat(n, y)
+  180.0 / Math::PI * Math.atan(Math.sinh(Math::PI * (1.0 - 2.0 * y / n)))
+end
+
+def in_coverage?(z, x, y)
+  # coverage area: 56 degrees South to 60 degrees North
+  #return (lat(2 ** z, y) < 60) && (lat(2 ** z, y + 1) > -56)
+  return (lat(2 ** z, y) > -56) && (lat(2 ** z, y + 1) < 60)
+end
+
+1.upto(10) {|z|
   n = 2 ** z
   n.times {|x|
     n.times {|y|
       count += 1
       dir = "#{z}/#{x}"
       path = "#{z}/#{x}/#{y}.csv"
-      url = "http://example.com/path/#{path}"
-      # FileUtils.mkdir_p(dir) unless File.directory?(dir)
-      # File.write(path, open(url).read)
+      url = "http://earthexplorer.usgs.gov/wms/wmts/EPSG3857/srtm/#{path}"
       print "#{count}: #{url}\n"
+      next unless in_coverage?(z, x, y)
+      next if File.exist?(path)
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+      File.write(path, open(url).read)
+      sleep(rand / 3)
     }
   }
 }
